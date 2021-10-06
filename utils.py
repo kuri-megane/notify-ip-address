3aaf5870a3170d3c9be0313bdcb48e1948b3caa7
kuri.megane my_mac <kuri.megane3060.4@gmail.com>
2021-10-07 01:04:00 +0900

    グローバルipアドレスの取得を追加

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from netifaces import interfaces, ifaddresses, AF_INET
 import subprocess
+import requests
 
 
 def get_local_ip_address():
@@ -7,6 +8,11 @@ def get_local_ip_address():
     for ifaceName in interfaces():
         addresses.append([i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr': 'No IP addr'}])])
     return addresses
+
+
+def get_global_ip_address():
+    res = requests.get('http://inet-ip.info/ip')
+    return res.text
 
 
 def get_disk_usage():
